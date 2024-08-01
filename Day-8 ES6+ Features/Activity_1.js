ea9aae5b6aef8049eb817512aa3fb5e163ac8586
Dipankar Ganguly <dipankarganguly15@gmail.com>
2024-08-01 07:21:47 +0530

    restore

--- Day-8 ES6+ Features/Activity_1.js
+++
@@ -1,9 +0,0 @@
-/*
-  Activity 1: Template Literals :-
-      Task 1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console.
-      Task 2: Create a multi-line string using template literals and log it to the console.
-*/
-
-console.log(`Name is ${'dipankar'} age is ${21}`);
-
-console.log(`Name is ${'dipankar'} age is ${21}\Name is ${'dipankar'} age is ${21}\Name is ${'dipankar'} age is ${21}`);
